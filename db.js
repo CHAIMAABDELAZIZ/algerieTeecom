d163883452d6b69b3d682958915b449e0b46d956
CHAIMA <91737328+CHAIMAABDELAZIZ@users.noreply.github.com>
2024-09-17 21:48:04 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
-// Créer un pool de connexions à la base de données
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'AlgerieTelecom',
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Obtenir une connexion du pool
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Erreur de connexion :', err);
-        return;
-    }
-    console.log('Connecté avec succès');
-    // Libérer la connexion après utilisation
-    connection.release();
-});
+pool.getConnection((err, conn) => {
+    if(err) console.log(err)
+    console.log("Connected successfully")
+})
 
-// Exporter le pool pour utilisation dans d'autres modules
-module.exports = pool.promise();
+module.exports = pool.promise()
